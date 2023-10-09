2ae0383e108881851a0fda98962ec03131a25cd5
Magnus Bakke <eucmpau001@eucnordvest.dk>
2023-10-09 10:21:12 +0200

    Login program security update

--- introWebsite/PythonFolder/loginProgram.py
+++ introWebsite/PythonFolder/loginProgram.py
@@ -1,14 +1,46 @@
+import time, sys
+
 Username='Magnus'
 Password='1234'
+i=3
 
 print('Please enter your username')
-usernameinput=input()
-if usernameinput==Username:
-    print('Please enter password')
-    passwordinput=input()
-    if passwordinput==Password:
-        print('Access Granted')
+while True:
+    usernameInput=input()
+    if usernameInput != Username:
+        print('Account not found')
+        continue
     else:
-        print('Access Denied')
-else:
-    print('Username not found')
+        print('Enter password')
+        while True:
+            if i==0:
+                print('Your account has been locked')
+                sys.exit()
+            else:
+                passwordInput=input()
+                if passwordInput!=Password:
+                    i=i-1
+                    print('Wrong password')
+                    print('You have ' + str(i) +' attempts left')
+                    continue
+                else:
+                    break
+    break
+print('Access Granted')
+
+
+
+
+
+
+# old code
+# usernameinput=input()
+# if usernameinput==Username:
+#     print('Please enter password')
+#     passwordinput=input()
+#     if passwordinput==Password:
+#         print('Access Granted')
+#     else:
+#         print('Access Denied')
+# else:
+#     print('Username not found')
