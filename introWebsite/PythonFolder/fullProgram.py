c8b9827f3a8a57e92296f2a74c5f482723085b9c
Magnus Bakke <eucmpau001@eucnordvest.dk>
2023-10-11 11:04:53 +0200

    Full program updated

--- introWebsite/PythonFolder/fullProgram.py
+++ introWebsite/PythonFolder/fullProgram.py
@@ -5,6 +5,7 @@ Password='1234'
 i=3
 usernameInput=''
 passwordInput=''
+userInput=''
 
 print('Please enter your username')
 while usernameInput != Username:
@@ -27,70 +28,93 @@ while passwordInput!=Password:
             print('Attempts remaining: ' + str(i))
 
 print('Access Granted')
+print('___________________')
 time.sleep(0.2)
 print('You now have access to your persoanl dashboard')
 time.sleep(0.5)
 print('Do you want to convert DKK to Euro, or would you like to calculate your postal fee?')
+print('___________________')
 time.sleep(0.5)
-print('Enter "C" to convert DKK to Euro')
-print('Enter "P" to calculate postal fee')
-print('Enter "exit" to log out')
 
-userInput=input()
+while userInput != 'x':
+    print('Enter "c" to convert DKK to Euro')
+    print('Enter "p" to calculate postal fee')
+    print('Enter "x" to log out and exit')
+    print('___________________')
 
-if userInput=='C':
-    kurs=0.13
+    userInput=input()
 
-    print('How many DKK would you like to exchange for Euro?')
-    DKKInput=input()
-    print('exchange is subject to 2% fee, minimum 0.5 euro')
-    euroOutput=int(DKKInput)*kurs
-    fee=euroOutput/100*2
-    if fee<0.5:
-        fee=0.5
-    else:
-        fee=fee
-    finalEuroAmount=euroOutput-fee
-    print('You will receive '+str(int(finalEuroAmount))+' euro and you will pay '+str(float(fee))+' euro in fees.')
-    print('Thank your for your business.')
-
-elif userInput=='P':
-    niveau1=25
-    niveau1pris=10
-    niveau2=50
-    niveau2=18
-    niveau3=100
-    niveau3pris=25
-
-    print('Hej, jeg er din personlige post-assistent, og kan hjælpe dig med at udregne porto når du vil sende breve.')
-    time.sleep(1)
-    print('Hvad er vægten på dit brev?')
-
-    while True:
-        weight=input()
-        try:
-            weight = float(weight)
-        except:
-            print ('Indtast et tal')
-            continue
-        break 
-
-    if weight<=niveau1:
-        print('Your price will be ' + str(niveau1pris) + ' Kr.')
-    elif weight<=niveau2:
-        print('Your price will be ' + str(niveau2pris) + ' Kr.')
-    elif weight<=niveau3:
-        print('Your price will be ' + str(niveau3pris) + ' Kr.')
-    elif weight>niveau3:
-        print('Din pakke er for tung til at blive sendt som brev.')
-
-    time.sleep(1)
-    print('Du må have en god dag')
+    if userInput=='c':
+        while userInput != 'n':
+            kurs=0.13
+            print('How many DKK would you like to exchange for Euro?')
+            while True:
+                DKKInput=input()
+                try:
+                    DKKInput = float(DKKInput)
+                except:
+                    print ('Enter a valid number')
+                    continue
+                break 
+            print('exchange is subject to 2% fee, minimum 0.5 euro')
+            euroOutput=int(DKKInput)*kurs
+            fee=euroOutput/100*2
+            if fee<0.5:
+                fee=0.5
+            else:
+                fee=fee
+            finalEuroAmount=euroOutput-fee
+            time.sleep(0.2)
+            print('___________________')
+            print('You will receive '+str(int(finalEuroAmount))+' euro and you will pay '+str(float(fee))+' euro in fees.')
+            time.sleep(0.2)
+            print('Thank your for your business.')
+            print('___________________')
+            time.sleep(0.5)
+            print('If you want to convert again tap enter')
+            print('If you want to return to dashboard enter "n"')
+            userInput=input()
 
 
-elif userInput=='exit':
-    print('You are now logged out')
-    sys.exit()
+    elif userInput=='p':
+        while userInput != 'n':
+            niveau1=25
+            niveau1pris=10
+            niveau2=50
+            niveau2=18
+            niveau3=100
+            niveau3pris=25
+
+            print('Hello, I am your personal shipping cost assistant.')
+            time.sleep(0.3)
+            print('What is the weight of your letter?')
+
+            while True:
+                weight=input()
+                try:
+                    weight = float(weight)
+                except:
+                    print ('Indtast et tal')
+                    continue
+                break 
+            time.sleep(0.2)
+            if weight<=niveau1:
+                print('Your price will be ' + str(niveau1pris) + ' Kr.')
+            elif weight<=niveau2:
+                print('Your price will be ' + str(niveau2pris) + ' Kr.')
+            elif weight<=niveau3:
+                print('Your price will be ' + str(niveau3pris) + ' Kr.')
+            elif weight>niveau3:
+                print('Your package is too heavy to send as a letter.')
+
+            print('___________________')
+            time.sleep(0.5)
+            print('Thank you for your business!')
+            print('___________________')
+            time.sleep(0.3)
+            print('If you want to calculate postal shipping again tap enter')
+            print('If you want to return to dashboard enter "n"')
+            userInput=input()
 
 print('You have been logged out')
 
